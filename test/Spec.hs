b69d7c9b74b860a9938d7948540ef2a82cdc0095
Dilawar Singh <dilawars@ncbs.res.in>
2016-08-18 14:16:51 +0530

    other implementation of kronecker function. One test is passing.

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,16 +1,19 @@
 import qualified Data.Eigen.Matrix as E
 import Data.Eigen.Util
 
-testKronecker m1 m2 = kronecker m1 m2
 
 mat1 = E.fromList [ [1, 2], [3, 4] ] :: E.MatrixXd 
 mat2 = E.fromList [ [5, 6], [7, 8] ] :: E.MatrixXd 
+res = E.fromList [ [5,6,10,12], [7,8,14,16], [15, 18, 20, 24], [21, 24, 28, 32 ] ] :: E.MatrixXd
+
+testKronecker m1 m2 = do 
+    pprintIO m1
+    pprintIO m2
+    pprintIO res
+    pprintIO $ kronecker m1 m2
+    putStrLn "Test done"
 
 main :: IO ()
 main = do 
-    pprintIO mat1
-    pprintIO mat2
-    let res = kronecker mat1 mat2
-    {-pprintIO res-}
-    print res
+    testKronecker mat1 mat2 
     putStrLn "All done"
